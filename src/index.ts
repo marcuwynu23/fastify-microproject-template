5328840e5b939e0f8c6277d4d8dc17e1236a7993
Mark Wayne Menorca <marcuwynu23@gmail.com>
2025-05-03 13:34:30 +0800

    chore: adds logging functionality and updates dependencies
    Introduces logging to a file using a log stream
    Updates the 'fastify' package to version 5.3.2
    Removes unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import Fastify, { FastifyInstance } from "fastify";
 import process from "process";
+import fs from "fs";
 import route from "./routes/route";
-const fastify: FastifyInstance = Fastify({ logger: true });
 
+// Create a log stream for the file
+const logFileStream = fs.createWriteStream("./logs/app.log", { flags: "a" });
+
+// Create the Fastify instance with custom logger configuration
+const fastify: FastifyInstance = Fastify({
+  logger: {
+    level: "debug",
+    stream: logFileStream,
+    safe: true,
+  },
+});
+
+// Register the routes
 fastify.register(route);
+
 // Start the server
 const start = async () => {
   try {
     await fastify.listen({ port: 4000 });
-    console.log("Server running at http://localhost:4000");
+    fastify.log.info("Server running at http://localhost:4000");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
