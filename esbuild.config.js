fcbfbfec48052bc3bf48554ab3926fd7ec99f9ae
Mark Wayne Menorca <marcuwynu23@gmail.com>
2025-08-07 17:44:20 +0800

    refactor: enhance esbuild config with performance timing for bundling

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,6 +1,9 @@
 const {build} = require("esbuild");
 const alias = require("esbuild-plugin-alias");
 const path = require("path");
+const {performance} = require("perf_hooks");
+
+const start = performance.now();
 
 build({
   entryPoints: ["src/index.ts"],
@@ -12,13 +15,15 @@ build({
   sourcemap: false,
   plugins: [
     alias({
-      "@": path.resolve(__dirname, "src"), // <== Adjust this if your alias points elsewhere
+      "@": path.resolve(__dirname, "src"),
     }),
   ],
   resolveExtensions: [".ts", ".js", ".json"],
 })
   .then(() => {
-    console.log("✅ esbuild bundling completed.");
+    const end = performance.now();
+    const time = (end - start).toFixed(2);
+    console.log(`✅ esbuild bundling completed in ${time}ms`);
   })
   .catch((err) => {
     console.error("❌ esbuild bundling failed:", err);
